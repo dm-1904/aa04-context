001c052333d6ca93194a73cc1f5909b359edcd46
atxmikaela <mikaela@mikaela@mikaelataylor.com>
2024-04-30 23:20:30 -0500

    finished 10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,6 +1,33 @@
-// Your code here 
+class PartyPlanner {
+	constructor(guestList = []){
+		this.guestList = guestList
+	}
 
+	addToGuestList(name){
+		this.guestList.push(name)
+	}
 
+	throwParty(){
+		if (!this.guestList.length){
+			return("Gotta add people to the guest list")
+		}
+		if (this.guestList.length === 1) {
+			return(`Welcome to the party ${this.guestList[0]}`)
+		} else {
+			return(`Welcome to the party ${this.guestList[0]} and ${this.guestList[1]}`)
+		}
+	}
+}
+
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
