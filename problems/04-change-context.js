75365b87afa7ac16d50b797d4d67ddff919bf618
atxmikaela <mikaela@mikaela@mikaelataylor.com>
2024-04-30 22:01:58 -0500

    four done

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,7 +1,27 @@
 function changeContext(func, obj) {
-  // Your code here 
+  // return func.call(obj)
+
+ const bound = func.bind(obj)
+ return bound();
+
+
+  }
+
+
+
+
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 }
 
+function extractName() {
+  return this.name;
+}
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
