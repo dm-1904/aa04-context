392998c84478c0a91d637e138252b50283476b5a
damonryon <damon.ryon@gmail.com>
2024-04-30 19:35:42 -0700

    03 done

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,6 +1,31 @@
-// Your code here 
+class Dog{
+	constructor(name){
+		this.name = name
+	}
+	static makeJet(){
+		const jet = new Dog('Jet')
+		return jet
+	}
+	changeName(newName){
+		this.name = newName
+		// return this.name
+	}
+	speak(word){
+		this.word = word
+		return `${this.name} says ${this.word}`
+	}
+}
 
+let dog1 = Dog.makeJet(); // returns an object
 
+console.log(dog1.name);                 // Jet
+console.log(dog1.speak("hello"));       // Jet says hello
+console.log(dog1.changeName("Freyja")); // Freyja
+console.log(dog1.name);                 // Freyja
+console.log(dog1.speak("hello"));       // Freyja says hello
+
+let dog2 = Dog.makeJet();
+console.log(dog2.name);                 // Jet
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
